2969d1124a41cdaee0f83b122fce815c37686b3c
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-03-06 21:32:12 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/graduatework/GraduateWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.graduatework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduateWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
